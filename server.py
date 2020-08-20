ab4cb4442a43afaef5f09731ea877bfcbecb24bc
Lewis Gaul <legaul@cisco.com>
2020-08-20 10:12:30 +0100

    Format with black

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
-import sanic
 import functools
+
+import sanic
 
 app = sanic.Sanic("hello_example")
 
-app.static('/', 'build/')
+app.static("/", "build/")
+
 
 def check_request_for_authorization_status(request):
     sanic.log.logger.info(f"Checking authorization: IP is {request.ip}")
-    match = (request.ip == "127.0.0.1")
+    match = request.ip == "127.0.0.1"
     sanic.log.logger.info(f"Check IP matches 127.0.0.1: {match}")
 
     return match
+
 
 def authorized(f):
     @functools.wraps(f)
@@ -23,14 +26,16 @@ def authorized(f):
             return response
         else:
             # The user is not authorized, so respond with 403.
-            return sanic.response.json({'status': 'not_authorized'}, 403)
+            return sanic.response.json({"status": "not_authorized"}, 403)
 
     return decorated_function
+
 
 @app.route("/")
 @authorized
 async def index(request):
-  return await sanic.response.file("build/index.html")
+    return await sanic.response.file("build/index.html")
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
