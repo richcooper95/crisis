0896f569a6eed5796fad6d63b59f609117b38c46
Matthew Hall <github@matthall.co.uk>
2021-01-02 23:18:51 +0000

    Use AWS Amplify to host the frontend.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {AmplifyAuthenticator, AmplifySignIn, AmplifySignOut} from '@aws-amplify/ui-react';
 import './index.css';
 import Sidebar from './components/Sidebar';
 import Arena from './components/Arena';
@@ -22,32 +23,36 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        <Sidebar
-          onSidebarSelect={s => this.handleSidebarSelect(s)}
-          sidebarSelect={this.state.sidebarSelect}
-        />
-        <div className="app-title">
-          <h1>
-            Crisis Coach Assignment Tool
-          </h1>
+      <AmplifyAuthenticator>
+        <AmplifySignIn slot="sign-in" hideSignUp={true} />
+        <div className="app">
+          <Sidebar
+            onSidebarSelect={s => this.handleSidebarSelect(s)}
+            sidebarSelect={this.state.sidebarSelect}
+          />
+          <div className="app-title">
+            <h1>
+              Crisis Coach Assignment Tool
+            </h1>
+          </div>
+          <div className="app-title-image" >
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://crisis.org.uk"
+            >
+              <img
+                src="logo-crisis.png"
+                alt="Crisis title logo"
+                height="80"
+                style={{float: "right"}}
+              />
+            </a>
+          </div>
+          <Arena sidebarSelect={this.state.sidebarSelect}/>
         </div>
-        <div className="app-title-image" >
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://crisis.org.uk"
-          >
-            <img
-              src="logo-crisis.png"
-              alt="Crisis title logo"
-              height="80"
-              style={{float: "right"}}
-            />
-          </a>
-        </div>
-        <Arena sidebarSelect={this.state.sidebarSelect}/>
-      </div>
+        <AmplifySignOut/>
+      </AmplifyAuthenticator>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
 import './index.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
+Amplify.configure(awsmobile);
 
 // ========================================
 
