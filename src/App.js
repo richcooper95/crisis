b0fb54b4aee8be051ab2a27d67d87003d7451024
Matthew Hall <github@matthall.co.uk>
2021-04-30 11:25:48 +0100

    Don't require authentication in local dev instance (take two).

--- src/App.js
+++ src/App.js
@@ -22,37 +22,45 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <AmplifyAuthenticator>
-        <AmplifySignIn slot="sign-in" hideSignUp={true} />
-        <div className="app">
-          <Sidebar
-            onSidebarSelect={s => this.handleSidebarSelect(s)}
-            sidebarSelect={this.state.sidebarSelect}
-          />
-          <div className="app-title">
-            <h1>
-              Crisis Coach Assignment Tool
-            </h1>
-          </div>
-          <div className="app-title-image" >
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://crisis.org.uk"
-            >
-              <img
-                src="logo-crisis.png"
-                alt="Crisis title logo"
-                height="80"
-                style={{float: "right"}}
-              />
-            </a>
-          </div>
-          <Arena sidebarSelect={this.state.sidebarSelect}/>
+    const app =
+      <div className="app">
+        <Sidebar
+          onSidebarSelect={s => this.handleSidebarSelect(s)}
+          sidebarSelect={this.state.sidebarSelect}
+        />
+        <div className="app-title">
+          <h1>
+            Crisis Coach Assignment Tool
+          </h1>
         </div>
-        <AmplifySignOut/>
-      </AmplifyAuthenticator>
-    )
+        <div className="app-title-image" >
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://crisis.org.uk"
+          >
+            <img
+              src="logo-crisis.png"
+              alt="Crisis title logo"
+              height="80"
+              style={{float: "right"}}
+            />
+          </a>
+        </div>
+        <Arena sidebarSelect={this.state.sidebarSelect}/>
+      </div>
+    ;
+
+    if (process.env.NODE_ENV === "production") {
+      app =
+        <AmplifyAuthenticator>
+          { process.env.NODE_ENV === "production" &&
+            <AmplifySignIn slot="sign-in" hideSignUp={true} /> }
+          {app}
+          { process.env.NODE_ENV === "production" && <AmplifySignOut/> }
+        </AmplifyAuthenticator>;
+    }
+
+    return(app);
   }
 }
